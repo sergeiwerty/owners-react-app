ab7fb243a510329da73f50ae715ee80c75bb2408
sergeiwerty <dshl.srp@gmail.com>
2021-02-15 17:47:07 +0300

    layout finishing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react'
 import { data } from './ownersData'
 import './App.css'
-import OwnersList from './components/OwnersList'
+import OwnersList from './components/Owners/OwnersList'
 import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   const [owners, setOwners] = useState(data)
   return (
     <main>
+      <Sidebar />
       <section className='container'>
-        <h2>Overview</h2>
-        <Sidebar />
         <OwnersList owners={owners} />
         <button onClick={() => setOwners(data)}>Ничего не произойдёт</button>
       </section>
