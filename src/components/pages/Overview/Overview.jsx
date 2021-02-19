f3d7ad02f3f0fce1fc7ac2c53d753debd194c73a
Sergey D <dshl.srp@gmail.com>
2021-02-19 21:32:02 +0300

    add user functionality

--- src/components/pages/Overview/Overview.jsx
+++ src/components/pages/Overview/Overview.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Overview.module.css'
 // import { owners } from '../../../ownersData'
 
 const Overview = ({ owners }) => {
+  const [show, setShow] = useState(false)
   return (
     <>
       <div className={styles.owners_heading}>
-        <h2>Overview</h2>
+        {!show && <h2>Overview</h2>}
+        {show && <AddForm />}
         <div>
           <button>...</button>
-          <button>Add</button>
+          <button className='add_btn' onClick={() => setShow(!show)}>
+            Add
+          </button>
         </div>
       </div>
       <p className={styles.lines_heading}>
@@ -37,6 +41,39 @@ const Overview = ({ owners }) => {
         )
       })}
     </>
+  )
+}
+
+function AddForm() {
+  return (
+    <form action='' className={styles.add_form}>
+      <div className={styles.iputs_wrapper}>
+        <label htmlFor='fullName'>
+          Full name:
+          <input type='text' id='fullName' name='fullName' />
+        </label>
+
+        <label htmlFor='endDate'>
+          End date:
+          <input type='text' id='endDate' name='endDate' />
+        </label>
+      </div>
+      <div className={styles.iputs_wrapper}>
+        <label htmlFor='profits'>
+          Profits:
+          <input type='text' id='profits' name='profits' />
+        </label>
+
+        <label htmlFor='losses'>
+          Losses:
+          <input type='text' id='losses' name='losses' />
+        </label>
+        <label htmlFor='phone'>
+          Phone:
+          <input type='text' id='phone' name='phone' />
+        </label>
+      </div>
+    </form>
   )
 }
 
